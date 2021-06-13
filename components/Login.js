b771bf320fe307dddd5ca3a3fe693841a84c2eb7
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2021-06-13 18:16:37 +0800

    Add formik and yup ad update login page

--- components/Login.js
+++ components/Login.js
@@ -1,19 +1,31 @@
+import React, {useState} from 'react'
 import {
   Flex,
-  Box,
   FormControl,
   FormLabel,
   Input,
-  Checkbox,
   Stack,
   Link,
   Button,
   Heading,
   Text,
+  Box,
   useColorModeValue,
-} from '@chakra-ui/react';
+} from '@chakra-ui/react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+const LoginSchema = Yup.object().shape({
+  username: Yup.string()
+    .required('Username is required'),
+  password: Yup.string()
+    .required('Password is required')
+})
 
 export default function SimpleCard() {
+  const [loading, setLoading] = useState(false)
+  const [networkErrors, setNetwokErrors] = useState(false)
+
   return (
     <Flex
       minH={'100vh'}
@@ -27,27 +39,57 @@ export default function SimpleCard() {
             to enjoy all of our cool <Link color={'blue.400'}>blogs</Link> ✌️
           </Text>
         </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Username</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Button>
-                Sign in
-              </Button>
-            </Stack>
-          </Stack>
-        </Box>
+        <Formik
+          initialValues={{
+            username: '',
+            password: ''
+          }}
+          validationSchema={LoginSchema}
+          onSubmit={(values, actions) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2))
+              actions.setSubmitting(false)
+            }, 1000)
+          }}
+        >
+          {formik => (
+            <Form
+              rounded={'lg'}
+              bg={useColorModeValue('white', 'gray.700')}
+              p={8}
+            >
+              <Stack spacing={4}>
+                <Field as="input" name="username">
+                  {({field, form}) => (
+                    <FormControl id="username" isInvalid={form.errors.username && form.touched.username}>
+                      <FormLabel>Username</FormLabel>
+                      <Input {...field} />
+                      <ErrorMessage name="username">
+                        {msg => <Text fontSize="sm" pt="1.5" color="red.500">{msg}</Text>}
+                      </ErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field as="input" name="password">
+                  {({field, form}) => (
+                    <FormControl id="password" isInvalid={form.errors.password && form.touched.password}>
+                      <FormLabel>Password</FormLabel>
+                      <Input {...field} type="password" />
+                      <ErrorMessage name="password">
+                        {msg => <Text fontSize="sm" pt="1.5" color="red.500">{msg}</Text>}
+                      </ErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Stack spacing={10}>
+                  <Button type="submit" isLoading={formik.isSubmitting}>
+                    Sign in
+                  </Button>
+                </Stack>
+              </Stack>
+            </Form>
+          )}
+        </Formik>
       </Stack>
     </Flex>
   );
