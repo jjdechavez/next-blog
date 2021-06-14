c917098bffc331d2e556d0d18875d4d2dbfb2abf
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2021-06-14 14:47:32 +0800

    Remove posts lib

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Layout from '../../components/layout'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
-import { getAllPostIds, getPostData } from '../../lib/posts'
 import { Center } from '@chakra-ui/react'
 
 export default function Post({ blog }) {
