47981f42125d8495dc67b671305cc585c7faa122
John Jerald De Chavez <jjdechavez.techmaker@gmail.com>
2021-06-15 10:31:44 +0800

    Add create blog on profile

--- pages/profile.js
+++ pages/profile.js
@@ -13,13 +13,39 @@ import {
   Text,
   Stack,
   Button,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Input,
+  FormControl,
+  FormLabel,
   useColorModeValue,
+  useDisclosure
 } from '@chakra-ui/react';
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+const initialValues = {
+  name: '',
+  description: ''
+}
+
+const CreateBlogSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Name is required'),
+  description: Yup.string()
+    .required('Description is required'),
+})
 
 export default function Profile() {
   const token = getToken()
   const [getUserLoading, getUserError, user] = useUser(token);
-  const [getUserBlogsLoading, getUserBlogsError, blogs] = useUserBlogs(token);
+  const [getUserBlogsLoading, getUserBlogsError, blogs, {refetchBlogs}] = useUserBlogs(token);
+  const {isOpen, onOpen, onClose} = useDisclosure()
 
   if (getUserLoading || !user || getUserBlogsLoading, !blogs) {
     return <>Loading...</>
@@ -72,6 +98,7 @@ export default function Profile() {
           </Text>
           <Stack mt={8} direction={'row'} spacing={4}>
             <Button
+              onClick={onOpen}
               flex="1"
               fontSize={'sm'}
               rounded={'full'}
@@ -91,6 +118,92 @@ export default function Profile() {
           </Stack>
         </Box>
       </Center>
+      <Drawer isOpen={isOpen} onClose={onClose} placement="bottom">
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>New Blog</DrawerHeader>
+          <DrawerBody>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={CreateBlogSchema}
+              onSubmit={async (values, actions) => {
+                try {
+                  const response = await fetch("http://localhost:5000/todos", {
+                    method: 'POST',
+                    headers: { 
+                      'Content-Type': 'application/json',
+                      authorization: token
+                    },
+                    body: JSON.stringify({
+                      name: values.name,
+                      description: values.description
+                    })
+                  })
+
+                  if (response.status === 201) {
+                    refetchBlogs(token)
+                    onClose()
+                  }  
+                  
+                  if (response.status === 500) {
+                    console.log("Create Blog failed")
+                    let error = new Error(response.statusText)
+                    error.response = response
+                    throw error
+                  }
+
+                } catch (error) {
+                  console.error(
+                    "You have an error in your code or there are network issues.",
+                    error
+                  )
+                } finally {
+                  actions.setSubmitting(false)
+                }
+              }}
+            >
+              {formik => (
+                <Form
+                  id="create-blog-form"
+                  rounded={'lg'}
+                  bg={useColorModeValue('white', 'gray.700')}
+                  p={8}
+                > <Stack spacing={4}>
+                    <Field as="input" name="name">
+                      {({field, form}) => (
+                        <FormControl id="name" isInvalid={form.errors.name && form.touched.name}>
+                          <FormLabel>Name</FormLabel>
+                          <Input {...field} />
+                          <ErrorMessage name="name">
+                            {msg => <Text fontSize="sm" pt="1.5" color="red.500">{msg}</Text>}
+                          </ErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field as="input" name="description">
+                      {({field, form}) => (
+                        <FormControl id="description" isInvalid={form.errors.description && form.touched.description}>
+                          <FormLabel>Description</FormLabel>
+                          <Input {...field} />
+                          <ErrorMessage name="description">
+                            {msg => <Text fontSize="sm" pt="1.5" color="red.500">{msg}</Text>}
+                          </ErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                  </Stack>
+                </Form>
+              )}
+            </Formik>
+          </DrawerBody>
+          <DrawerFooter>
+            <Button type="submit" form="create-blog-form">
+              Save
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
       <Blogs />
     </Layout>
   );
