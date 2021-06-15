68689d1ef77bf71885b95aa1fa00317b0346d790
John Jerald De Chavez <jjdechavez.techmaker@gmail.com>
2021-06-15 21:54:04 +0800

    Add react-query and create custom hooks for fetching blogs

---
+++ components/blog/CardSkeleton.js
@@ -0,0 +1,39 @@
+import {
+  Box,
+  Center,
+  Skeleton,
+  Stack,
+  SkeletonCircle,
+  SkeletonText
+} from '@chakra-ui/react';
+
+
+export default function BlogPostSkeletons({ noOfSkeletons = 5 }) {
+  let skeletons = [];
+  let numbers = Array(noOfSkeletons);
+  for (let index = 0; index < numbers.length; index++) {
+    skeletons.push(
+      <Center py={6}>
+        <Box
+          w={'full'}
+          boxShadow={'xl'}
+          rounded={'md'}
+          p={6}
+          overflow={'hidden'}>
+          <Stack>
+            <Skeleton height="20px" width="40" />
+            <SkeletonText mt="4" noOfLines={2} spacing="2" />
+          </Stack>
+          <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
+            <SkeletonCircle size="10" />
+            <Stack direction={'column'} width="20">
+              <SkeletonText noOfLines={2} />
+            </Stack>
+          </Stack>
+        </Box>
+      </Center>
+    );
+  }
+
+  return skeletons;
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import '../styles/global.css'
 import { ChakraProvider } from "@chakra-ui/react"
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
