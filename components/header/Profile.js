4ebb0ec6ff441012dd8e866015f8bab27cbf2470
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2021-06-16 19:37:33 +0800

    Revert on profile header, use the custom hook

--- components/header/Profile.js
+++ components/header/Profile.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Router from 'next/router'
+import { useQueryClient } from 'react-query'
 import {Menu, MenuButton, Button, MenuList, MenuItem} from '@chakra-ui/react'
 import {ChevronDownIcon, InfoOutlineIcon, ArrowBackIcon} from '@chakra-ui/icons'
 import {logout} from '../../lib/auth'
 
 export default function ProfileHeader({ profile, loading }) {
+  const queryClient = useQueryClient()
   const logoutUser = () => {
+    queryClient.invalidateQueries('user')
     logout()
   }
 
@@ -13,7 +16,18 @@ export default function ProfileHeader({ profile, loading }) {
     Router.push('/profile')
   }
 
-  if (loading || !profile) return <>Loading...</>
+  if (loading) return <>Loading...</>
+
+  if (!profile) {
+    toast({
+      title: error.response.data.error,
+      description: "Please login.",
+      status: "error",
+      duration: 9000,
+      isClosable: true,
+    })
+    router.push('/login')
+  }
 
   return (
     <Menu>
